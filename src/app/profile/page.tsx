ae7c21c3b4018a266362e098169e828563175a92
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-08-13 00:19:26 +0000

    feature-frontend-userprofile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function profilePage(){
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile Page</h1>
+            <hr />
+            <p>Welcome to your profile page</p>
+            
+        </div>
+    )
+}
